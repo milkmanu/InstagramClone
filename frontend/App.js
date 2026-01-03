dae7a0c88749de94beb884203f7a7ac22ce6b803
milkmanu <168108017+milkmanu@users.noreply.github.com>
2026-01-03 16:28:08 +0500

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -124,7 +124,7 @@ export class App extends Component {
                 case 'Feed':
                 default: {
                   return {
-                    headerTitle: 'Instagram',
+                    headerTitle: 'crgram',
                   };
                 }
               }
